217bed81a243c8cc945237c00c60b1d4d1dd327b
Happy Verma <happyverma39274@gmail.com>
2026-04-05 20:25:14 +0530

    fix errors

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
+  console.error('MONGO_URI is not defined. Please set it in your environment variables.');
   throw new Error('Please define the MONGO_URI environment variable');
 }
 
