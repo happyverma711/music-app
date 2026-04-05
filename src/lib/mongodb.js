b73a55f82794ce9e4725b456f21a7efd60fbbb0c
Happy Verma <happyverma39274@gmail.com>
2026-04-05 19:10:03 +0530

    new commit

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,16 +1,11 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/musicplaylist';
+const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
   throw new Error('Please define the MONGO_URI environment variable');
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
 let cached = global.mongoose;
 
 if (!cached) {
